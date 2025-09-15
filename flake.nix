c21aed73516539f79f9a0dce176605d4b22d8869
uPiscium <upiscium@gmail.com>
2025-09-15 11:56:58 +0900

    Add test code

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          cudaSupport = true;
+        };
+      };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          uv
+          openssl
+          llvm_14
+          stdenv
+          cudatoolkit
+          # cudaPackages.cudatoolkit-legacy-runfile
+          nvidia-docker
+        ];
+        buildInputs = with pkgs; [
+          openssl
+          llvm_14
+          stdenv
+        ];
+        # LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.openssl}/lib:${pkgs.llvm_14}/lib";
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+          stdenv.cc.cc.lib
+          openssl
+          llvm_14
+          cudatoolkit
+          nvidia-docker
+          libGL
+          glib
+        ]);
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+      };
+    };
+}
+
--- src/entry.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-from app import App
-
-app = App().get_app()
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8002)
