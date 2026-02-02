275b49165146146e09dd1c555af6c21bb07fbb67
uPiscium <upiscium@gmail.com>
2026-02-02 09:51:20 +0900

    Update config and add Docker support

--- src/entry.py
+++ src/entry.py
@@ -9,32 +9,43 @@ parser.add_argument(
     "-p",
     "--port",
     type=int,
-    default=8002,
-    help="Port to run the FastAPI application on(default: 8002)",
+    default=None,
+    help="Port to run the FastAPI application on",
 )
 parser.add_argument(
     "-c",
     "--config",
     type=str,
-    default="./settings/config.json",
-    help="Config file path",
+    default="settings/config.json",
+    help="Path to the configuration file",
 )
 parser.add_argument(
     "-d",
     "--debug",
     action="store_true",
-    help="Enable debug mode",
+    default=None,
+    help="Run the application in debug mode",
 )
 parser.add_argument(
     "-l",
     "--logging",
     action="store_true",
-    help="Enable network logging",
+    default=None,
+    help="Enable logging",
 )
 args = parser.parse_args()
 
 with open(args.config, "r") as f:
     config = json.load(f)
+
+if args.port is None:
+    args.port = config.get("system", {}).get("port", 8002)
+
+if args.debug is None:
+    args.debug = config.get("system", {}).get("debug_mode", False)
+
+if args.logging is None:
+    args.logging = config.get("system", {}).get("enable_logging", False)
 
 app = App(config, args.debug, args.logging).get_app()
 
